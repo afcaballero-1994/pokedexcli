67eebd0493114ffd0f703e68e4939d92272aa27a
Andres Pedraza <andrespedca@gmail.com>
2025-10-16 16:25:38 -0500

    added basic caching

--- internal/pokeapi/pokeapi_methods.go
+++ internal/pokeapi/pokeapi_methods.go
@@ -6,9 +6,11 @@ import (
     "fmt"
     "io"
     "time"
+    "github.com/afcaballero-1994/pokedexcli/internal/pokecache"
 )
 
 const baseurl string = "https://pokeapi.co/api/v2"
+var cache = pokecache.NewCache(5 * time.Minute)
 
 type Client struct {
     client http.Client
@@ -26,31 +28,36 @@ func (c *Client) GetResources(pageURL *string) (ShallowMapResponse, error) {
     if pageURL != nil {
         url = *pageURL
     }
-    req, err := http.NewRequest("GET", url, nil)
+    data, exist := cache.Get(url)
+    if !exist {
+        req, err := http.NewRequest("GET", url, nil)
 
-    if err != nil {
-        fmt.Println(err)
-        return ShallowMapResponse{}, err
+        if err != nil {
+            fmt.Println(err)
+            return ShallowMapResponse{}, err
+        }
+
+        res, err := c.client.Do(req)
+        if err != nil {
+            return ShallowMapResponse{}, err
+        }
+
+        data, err = io.ReadAll(res.Body)
+        defer res.Body.Close()
+
+        if res.StatusCode > 299 {
+            fmt.Printf("received response with status: %d", res.StatusCode)
+        }
+
+        if err != nil {
+            return ShallowMapResponse{}, err
+        }
+        cache.Add(url, data)
     }
 
-    res, err := c.client.Do(req)
-    if err != nil {
-        return ShallowMapResponse{}, err
-    }
-
-    data, err := io.ReadAll(res.Body)
-    defer res.Body.Close()
-
-    if res.StatusCode > 299 {
-        fmt.Printf("received response with status: %d", res.StatusCode)
-    }
-
-    if err != nil {
-        return ShallowMapResponse{}, err
-    }
 
     var areas ShallowMapResponse = ShallowMapResponse{}
-    if err = json.Unmarshal(data, &areas); err != nil {
+    if err := json.Unmarshal(data, &areas); err != nil {
         return areas, err
     }
 
---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,66 @@
+package pokecache
+
+import (
+	"sync"
+    "time"
+    "fmt"
+)
+
+type cacheEntry struct {
+    createdAt time.Time
+    val []byte
+}
+
+type Cache struct {
+    entry map[string]cacheEntry
+    interval time.Duration
+    mu *sync.Mutex
+}
+
+func NewCache(t time.Duration) Cache {
+    c := Cache{
+        entry: map[string]cacheEntry{},
+        interval: t,
+        mu: &sync.Mutex{},
+    }
+    go c.reapLoop()
+    return c
+}
+
+func (c *Cache)Add(key string, val []byte) {
+    c.mu.Lock()
+    defer c.mu.Unlock()
+
+    fmt.Println("Adding Entry:", key)
+    c.entry[key] = cacheEntry{createdAt: time.Now(), val: val,}
+}
+
+func (c *Cache)Get(key string) ([]byte, bool) {
+    c.mu.Lock()
+    defer c.mu.Unlock()
+
+    fmt.Println("Getting url:", key)
+    data, exist := c.entry[key]
+    return data.val, exist
+}
+
+func (c *Cache)reapLoop() {
+
+    ticker := time.NewTicker(c.interval)
+    defer ticker.Stop()
+
+    c.mu.Lock()
+    c.mu.Unlock()
+
+    for {
+        select {
+            case t := <-ticker.C:
+                for k, v := range c.entry{
+                    diff := t.Sub(v.createdAt)
+                    if diff > c.interval {
+                        delete(c.entry, k)
+                    }
+                }
+        }
+    }
+}
